f91e6997db5d249c5c9c94d8907231f4224a6f9a
krishnaamen <krishnaament@gmail.com>
2022-03-02 06:05:42 +0100

    updating hyperlinks

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ app.use(express.static("public"));
 
 const fs = require("fs");
 
-const nav = fs.readFileSync("./public/components/nav.html").toString();
-const footer = fs.readFileSync("./public/components/footer.html").toString();
+//const nav = fs.readFileSync("./public/components/nav.html").toString();
+//const footer = fs.readFileSync("./public/components/footer.html").toString();
 
-const frontpage = fs.readFileSync("./public/pages/frontpage/frontpage.html").toString();
-const cheesegallery = fs.readFileSync("./public/pages/cheesegallery/cheesegallery.html").toString(); 
+const javascript = fs
+	.readFileSync("./public/pages/javascript/javascript.html")
+	.toString();
+const node = fs.readFileSync("./public/pages/node/node.html").toString();
 
-const frontpagePage = nav.replace("%%DOCUMENT_TITLE%%", "Welcome to the cheese zone") + frontpage + footer;
-const cheesegalleryPage = nav.replace("%%DOCUMENT_TITLE%%", "You are in the cheese zone") + cheesegallery + footer;
+//const frontpagePage = nav.replace("%%DOCUMENT_TITLE%%", "Welcome to the cheese zone") + frontpage + footer;
+//const cheesegalleryPage = nav.replace("%%DOCUMENT_TITLE%%", "You are in the cheese zone") + cheesegallery + footer;
 
-app.get("/", (req, res) => {
-    res.send(frontpagePage);
+app.get("/javascript", (req, res) => {
+	res.send(javascript);
 });
 
-app.get("/cheesegallery", (req, res) => {
-    res.send(cheesegalleryPage)
+app.get("/node", (req, res) => {
+	res.send(node);
 });
 
 app.listen(3000, () => console.log("Server is running on", 3000));
